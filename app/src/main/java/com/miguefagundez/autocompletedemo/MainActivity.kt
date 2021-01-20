b3880b6acf7697ac8f16db1004edd0475ed0eb62
Miguel <fagundezdev@gmail.com>
2021-01-20 07:51:50 -0500

    First version

---
+++ app/src/main/java/com/miguefagundez/autocompletedemo/MainActivity.kt
@@ -0,0 +1,58 @@
+package com.miguefagundez.autocompletedemo
+
+import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.AutoCompleteTextView
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    // Members
+    lateinit var autoCompleteTextView : AutoCompleteTextView
+    lateinit var textViewCountry : TextView
+    lateinit var listCountries : Array<String>
+    lateinit var adapter: ArrayAdapter<String>
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        setupMembers()
+        setupListCountries()
+        setupAdapter()
+        setupAutoComplete()
+        setupListeners()
+    }
+
+    private fun setupListeners() {
+        autoCompleteTextView.setOnItemClickListener{ adapterView: AdapterView<*>, view1: View, i: Int, l: Long ->
+            textViewCountry.text = adapter.getItem(i)
+        }
+
+    }
+
+    private fun setupAutoComplete() {
+        autoCompleteTextView.setAdapter(adapter)
+        autoCompleteTextView.threshold = 1
+    }
+
+    private fun setupAdapter() {
+        adapter = ArrayAdapter<String>(
+            applicationContext,
+            android.R.layout.simple_list_item_1,
+            listCountries
+        )
+    }
+
+    private fun setupListCountries() {
+        listCountries = resources.getStringArray(R.array.countries)
+    }
+
+    private fun setupMembers() {
+        autoCompleteTextView = findViewById(R.id.autoCompleteCountries)
+        textViewCountry = findViewById(R.id.tvCountryName)
+    }
+}
